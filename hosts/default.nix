681278108b83f008fdd4a8f3cb9cf1e361634452
Jose Victor Ferreira <root@josevictor.me>
2025-08-06 11:09:53 -0300

    updated, added lib folder for utils functions

--- hosts/default.nix
+++ hosts/default.nix
@@ -5,27 +5,8 @@ let
   importRoles =
     builtins.map (role: "${rolesPath}/${role}.nix") hostConfig.roles;
 
-  capitalize =
-    (s:
-      if s == "" then ""
-      else
-        let
-          head = lib.strings.toUpper (builtins.substring 0 1 s);
-          tail = builtins.substring 1 ((builtins.stringLength s) - 1) s;
-        in
-        "${head}${tail}");
-
-  toLowerCamelCase =
-    (s:
-      let
-        parts = lib.splitString "-" s;
-        first = lib.strings.toLower (builtins.head parts);
-        rest = map capitalize (builtins.tail parts);
-      in
-      lib.concatStringsSep "" ([ first ] ++ rest));
-
   mkRole = roleName: {
-    name = toLowerCamelCase roleName;
+    name = lib.strings.toLowerCamelCase roleName;
     value = { enable = true; };
   };
 
