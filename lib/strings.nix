239315e1f9fb8c06f3d4b1eddc03625e4a7c4219
Jose Victor Ferreira <root@josevictor.me>
2025-08-06 11:14:18 -0300

    fixed strings module

--- lib/strings.nix
+++ lib/strings.nix
@@ -1,38 +1,31 @@
 { lib }:
 
-let
-  inherit (lib.strings)
-    toLower toUpper splitString substring stringLength;
-in
 rec {
   /*
     capitalize "hello" -> "Hello"
     NOTE: Works on ASCII only – if you need full-Unicode case folding,
     use a separate implementation (the stock lib is ASCII-only too).
   */
-  capitalize = str:
-    if str == "" then str else
-    let
-      len = stringLength str;
-      first = toUpper (substring 0 1 str);
-    in
-    "${first}${substring 1 (len - 1) str}";
+  capitalize =
+    (s:
+      if s == "" then ""
+      else
+        let
+          head = lib.strings.toUpper (builtins.substring 0 1 s);
+          tail = builtins.substring 1 ((builtins.stringLength s) - 1) s;
+        in
+        "${head}${tail}");
 
   /*
     toLowerCamelCase "some-example_string value"
       -> "someExampleStringValue"
   */
-  toLowerCamelCase = raw:
-    let
-      # Split on space, dash, or underscore
-      words = lib.filter (s: s != "") (
-        lib.flatten (map (splitString "_") (map (splitString "-") (splitString " " raw)))
-      );
-
-      head = toLower (lib.head words);
-      tail = lib.concatMapStrings
-        (w: capitalize (toLower w))
-        (lib.tail words);
-    in
-    "${head}${tail}";
+  toLowerCamelCase =
+    (s:
+      let
+        parts = lib.splitString "-" s;
+        first = lib.strings.toLower (builtins.head parts);
+        rest = map capitalize (builtins.tail parts);
+      in
+      lib.concatStringsSep "" ([ first ] ++ rest));
 }
