c6e7f7173882c095ccc94ae00f8b33277fb2e301
Jose Victor Ferreira <root@josevictor.me>
2025-08-07 13:11:08 -0300

    updated k8snode defaults

--- modules/common/k8s-node-defaults.nix
+++ modules/common/k8s-node-defaults.nix
@@ -18,11 +18,46 @@ in
     environment.systemPackages = with pkgs; [
       cilium-cli
       fluxcd
+      ceph-client
+
+      (writeShellScriptBin "nuke-k3s" ''
+        if [ "$EUID" -ne 0 ] ; then
+          echo "Please run as root"
+          exit 1
+        fi
+        read -r -p 'Nuke k3s?, confirm with yes (y/N): ' choice
+        case "$choice" in
+          y|Y|yes|Yes) echo "nuke k3s...";;
+          *) exit 0;;
+        esac
+        flux uninstall -s || true
+        kubectl delete deployments --all=true -A
+        kubectl delete statefulsets --all=true -A  
+        kubectl delete ns --all=true -A
+        kubectl get ns | tail -n +2 | cut -d ' ' -f 1 | xargs -I{} kubectl delete pods --all=true --force=true -n {}
+        timeout 10 kubectl delete crds --all || true
+        cilium uninstall || true
+        echo "wait until objects are deleted..."
+        sleep 28
+        /run/current-system/sw/bin/k3s-killall.sh
+        systemctl stop k3s
+        sleep 2
+        rm -rf /var/lib/rancher/k3s/
+        rm -rf /var/lib/cni/networks/cbr0/
+        if [ -d /opt/k3s/data/temp ]; then
+          rm -rf /opt/k3s/data/temp/*
+        fi
+        sync
+        echo -e "\n => reboot now to complete k3s cleanup!"
+        sleep 3
+        reboot
+      '')
     ];
 
     boot.kernelModules = [
       "ceph"
       "rbd"
+      "nfs"
       "nft-expr-counter"
       "iptable_nat"
       "iptable_filter"
@@ -31,7 +66,10 @@ in
       "ip6table_mangle"
       "ip6table_raw"
       "ip6table_filter"
+      "ip_vs"
+      "ip_vs_rr"
     ];
+
     boot.kernel.sysctl = {
       "fs.inotify.max_user_instances" = 8192;
       "fs.inotify.max_user_watches" = 524288;
