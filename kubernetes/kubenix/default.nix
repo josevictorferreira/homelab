ba6a5c6d869e33dc58737a5e67396055f1ba63e9
Jose Victor Ferreira <root@josevictor.me>
2025-08-09 20:55:31 -0300

    refactoredkubenix generations, using subfolders now

--- kubernetes/kubenix/default.nix
+++ kubernetes/kubenix/default.nix
@@ -1,57 +1,60 @@
+# kubernetes/kubenix/default.nix
 { lib, kubenix, clusterConfig }:
 
 let
-  inherit (builtins) readDir toPath;
-  root = ./.;
+  isModuleFile = name:
+    lib.hasSuffix ".nix" name
+    && name != "default.nix"
+    && !(lib.hasPrefix "_" name);
 
-  discover = dir:
+  discover = dir: rel:
     let
-      entries = readDir dir;
+      entries = builtins.readDir dir;
       items = lib.mapAttrsToList (name: type: { inherit name type; }) entries;
 
-      files = lib.filter
-        (e: e.type == "regular"
-          && lib.hasSuffix ".nix" e.name
-          && e.name != "default.nix"
-          && !(lib.hasPrefix "_" e.name))
-        items;
-
+      files = lib.filter (e: e.type == "regular" && isModuleFile e.name) items;
       dirs = lib.filter (e: e.type == "directory") items;
 
       here = map
         (e: {
-          path = toPath "${dir}/${e.name}";
-          rel = lib.removePrefix (toString root + "/") "${dir}/${e.name}";
+          path = builtins.toPath "${dir}/${e.name}";
+          rel = if rel == "" then e.name else "${rel}/${e.name}";
         })
         files;
 
-      below = lib.concatMap (e: discover (toPath "${dir}/${e.name}")) dirs;
+      below = lib.concatMap
+        (e:
+          discover
+            (builtins.toPath "${dir}/${e.name}")
+            (if rel == "" then e.name else "${rel}/${e.name}")
+        )
+        dirs;
     in
     here ++ below;
 
-  modules = discover root;
+  modules = discover ./. "";
 
   evalModule = system: filePath:
     (kubenix.evalModules.${system} {
       modules = [ (import filePath) ];
-      specialArgs = {
-        inherit kubenix clusterConfig;
-      };
+      specialArgs = { inherit kubenix clusterConfig; };
     }).config.kubernetes.resultYAML;
 
   mkRenderer = system: pkgs:
     let
-      cmds = lib.concatStringsSep "\n" (map
-        (m: ''
-          mkdir -p "$out/${lib.dirOf m.rel}"
-          cp ${evalModule system m.path} "$out/${lib.removeSuffix ".nix" m.rel}.yaml"
-        '')
+      copyCmds = lib.concatStringsSep "\n" (map
+        (m:
+          let dest = "${lib.removeSuffix ".nix" m.rel}.yaml";
+          in ''
+            install -D ${evalModule system m.path} "$out/${dest}"
+          ''
+        )
         modules);
     in
     pkgs.runCommand "gen-k8s-manifests" { } ''
-      set -eu
+      set -euo pipefail
       mkdir -p "$out"
-      ${cmds}
+      ${copyCmds}
     '';
 in
 {
