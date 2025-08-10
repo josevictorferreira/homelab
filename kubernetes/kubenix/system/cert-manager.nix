2ea9c723bce995f7735dbe52782080689fdf3b1c
Jose Victor Ferreira <root@josevictor.me>
2025-08-10 16:33:52 -0300

    added namespace for cert-manager

--- kubernetes/kubenix/system/cert-manager.nix
+++ kubernetes/kubenix/system/cert-manager.nix
@@ -38,6 +38,12 @@ in
     };
 
     resources = {
+      namespaces.${namespace} = {
+        metadata = {
+          name = namespace;
+        };
+      };
+
       clusterissuer."cloudflare-issuer" = {
         metadata = {
           name = "cloudflare-issuer";
