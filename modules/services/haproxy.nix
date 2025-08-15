ee8b24cca40ee0548c32406550bc7bd3192e486b
Jose Victor Ferreira <root@josevictor.me>
2025-08-14 22:56:31 -0300

    added haproxy and keepalived to control nodes

---
+++ modules/services/haproxy.nix
@@ -0,0 +1,40 @@
+{ lib, clusterConfig, ... }:
+
+let
+  controlPlanes = builtins.map
+    (name:
+      let
+        host = clusterConfig.hosts.${name};
+      in
+      "${name} ${host.ipAddress}:6444"
+    )
+    clusterConfig.nodeGroups.k8sControlPlanes;
+in
+{
+  services.haproxy = {
+    enable = true;
+
+    config = ''
+      global
+        log stdout format raw local0
+        maxconn 4000
+
+      defaults
+        mode tcp
+        log global
+        option tcplog
+        timeout connect 5s
+        timeout client 50s
+        timeout server 50s
+
+      frontend kubernetes-api
+        bind *:6443
+        default_backend kubernetes-masters
+
+      backend kubernetes-masters
+        balance roundrobin
+        option tcp-check
+    '' + (lib.concatStringsSep "\n"
+      (map (s: "        server ${s} check") controlPlanes)) + "\n";
+  };
+}
