5b7e7a951a166b7448f6291dd13e500b31117835
Jose Victor Ferreira <root@josevictor.me>
2025-08-27 01:40:34 -0300

    updated ceph nfs

--- kubernetes/kubenix/storage/rook-ceph-nfs.nix
+++ kubernetes/kubenix/storage/rook-ceph-nfs.nix
@@ -179,11 +179,11 @@ in
                 awk -v newval="$SUBVOL_PATH" '{
                   gsub(/"path":[[:space:]]*"[^"]*"/, "\"path\": \"" newval "\"");
                   print
-                }' /etc/ganesha/export.json > /tmp/export.json
+                }' /tmp/export.json > /tmp/export_final.json
 
-                ceph -c "$CEPH_CONFIG" nfs export apply "$cluster" -i /tmp/export.json
+                ceph -c "$CEPH_CONFIG" nfs export apply "$cluster" -i /tmp/export_final.json
                 ceph -c "$CEPH_CONFIG" nfs cluster config reset "$cluster" || true
-                ceph -c "$CEPH_CONFIG" nfs cluster config set "$cluster" -i /etc/ganesha/custom.ganesha.conf
+                ceph -c "$CEPH_CONFIG" nfs cluster config set "$cluster" -i /tmp/custom.ganesha.conf
 
                 echo "--- CUSTOM CONFIGURATIONS ---"
                 rados -p .nfs --namespace ${nfsName} get "conf-nfs.${nfsName}"     /tmp/conf-nfs     || true
@@ -201,7 +201,7 @@ in
               { name = "mon-endpoints"; mountPath = "/etc/rook"; }
               { name = "ceph-config"; mountPath = "/etc/ceph"; }
               { name = "ceph-admin-secret"; mountPath = "/var/lib/rook-ceph-mon"; readOnly = true; }
-              { name = "${nfsName}-ganesha-custom-config"; mountPath = "/etc/ganesha"; readOnly = true; }
+              { name = "${nfsName}-ganesha-custom-config"; mountPath = "/tmp"; readOnly = true; }
             ];
           }];
           volumes = [
