75672dbb116d45200678bc18cd74f0946f3d2a81
Jose Victor Ferreira <root@josevictor.me>
2025-08-28 00:17:18 -0300

    updated ceph nfs

--- kubernetes/kubenix/storage/rook-ceph-nfs.nix
+++ kubernetes/kubenix/storage/rook-ceph-nfs.nix
@@ -109,7 +109,7 @@ in
               { key = "node-role.kubernetes.io/control-plane"; operator = "Exists"; effect = "NoSchedule"; }
             ];
           };
-          logLevel = "NIV_INFO";
+          logLevel = "NIV_FULL_DEBUG";
         };
       };
     };
