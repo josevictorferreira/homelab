bbaa5f6da755b29bd8c0cc41f27e3b04695513a7
Jose Victor Ferreira <root@josevictor.me>
2025-09-02 15:54:04 -0300

    creating a pv pointing to theshared storage

--- kubernetes/kubenix/apps/shared-storage-pvc.nix
+++ kubernetes/kubenix/apps/shared-storage-pvc.nix
@@ -1,12 +1,38 @@
 { homelab, ... }:
 
 let
-  pvName = "cephfs-shared-storage";
-  pvcName = "cephfs-shared-storage";
+  pvName = "cephfs-apps-shared-storage";
+  pvcName = "cephfs-apps-shared-storage";
+  fsName = "ceph-filesystem";
+  exportPath = "/volumes/nfs-exports/homelab-nfs/dfd23da6-d80d-48c7-b568-025ec7badd17";
   namespace = homelab.kubernetes.namespaces.applications;
 in
 {
   kubernetes.resources = {
+    persistentVolumes.${pvName} = {
+      metadata.name = pvName;
+      spec = {
+        capacity.storage = "1Gi";
+        accessModes = [ "ReadWriteMany" ];
+        persistentVolumeReclaimPolicy = "Retain";
+        volumeMode = "Filesystem";
+        csi = {
+          driver = "rook-ceph.cephfs.csi.ceph.com";
+          volumeHandle = pvName;
+          nodeStageSecretRef = {
+            name = "cephfs-user-secret";
+            namespace = namespace;
+          };
+          volumeAttributes = {
+            clusterID = "rook-ceph";
+            fsName = fsName;
+            staticVolume = "true";
+            rootPath = exportPath;
+          };
+        };
+      };
+    };
+
     persistentVolumeClaims.${pvcName} = {
       metadata = {
         name = pvcName;
