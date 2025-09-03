aa0c43789efda4547d352ec43baf9b74f9d99232
Jose Victor Ferreira <root@josevictor.me>
2025-09-03 13:24:27 -0300

    updated secrets

---
+++ kubernetes/kubenix/apps/postgresql.nix
@@ -0,0 +1,41 @@
+{ lib, kubenix, homelab, ... }:
+
+let
+  namespace = homelab.kubernetes.namespaces.applications;
+in
+{
+  kubernetes = {
+    helm.releases."postgresql" = {
+      chart = kubenix.lib.helm.fetch
+        {
+          chartUrl = "oci://registry-1.docker.io/bitnamicharts/postgresql";
+          chart = "postgresql";
+          version = "16.5.2";
+          sha256 = "sha256-YzN+udEKXR4P73M3sQ6RkRlLhNUunD/jv8C9Ve+Qsoo=";
+        };
+      includeCRDs = true;
+      noHooks = true;
+      namespace = namespace;
+      values = {
+        global.postgresql.auth = {
+          database = "linkwarden";
+          existingSecret = "postgresql-auth";
+          secretKeys = {
+            adminPasswordKey = "admin-password";
+            userPasswordKey = "user-password";
+            replicationPasswordKey = "replication-password";
+          };
+        };
+
+        primary.persistence = {
+          enabled = true;
+          storageClass = "rook-ceph-block";
+          reclaimPolicy = "Retain";
+          accessModes = [ "ReadWriteOnce" ];
+        };
+
+        service = kubenix.lib.plainServiceFor "postgresql";
+      };
+    };
+  };
+}
