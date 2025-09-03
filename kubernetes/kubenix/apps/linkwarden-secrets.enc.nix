8295ab9cb23d753d2a9456d31a160a18c364849d
Jose Victor Ferreira <root@josevictor.me>
2025-09-03 20:57:37 -0300

    added linkwarden

---
+++ kubernetes/kubenix/apps/linkwarden-secrets.enc.nix
@@ -0,0 +1,20 @@
+{ kubenix, homelab, ... }:
+
+let
+  namespace = homelab.kubernetes.namespaces.applications;
+in
+{
+  kubernetes = {
+    resources = {
+      secrets."linkwarden-secrets" = {
+        metadata = {
+          namespace = namespace;
+        };
+        data = {
+          "ANTHROPIC_MODEL" = kubenix.lib.secretsFor "anthropic_model";
+          "ANTHROPIC_API_KEY" = kubenix.lib.secretsFor "anthropic_api_key";
+        };
+      };
+    };
+  };
+}
