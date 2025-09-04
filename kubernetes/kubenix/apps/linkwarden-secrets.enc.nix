b50785b0d0750a8a7d8a41cb1aed32f52105a25a
Jose Victor Ferreira <root@josevictor.me>
2025-09-03 23:20:05 -0300

    updated linkwarden db secret

--- kubernetes/kubenix/apps/linkwarden-secrets.enc.nix
+++ kubernetes/kubenix/apps/linkwarden-secrets.enc.nix
@@ -13,6 +13,14 @@ in
         data = {
           "ANTHROPIC_MODEL" = kubenix.lib.secretsFor "anthropic_model";
           "ANTHROPIC_API_KEY" = kubenix.lib.secretsFor "anthropic_api_key";
+        };
+      };
+
+      secrets."linkwarden-db" = {
+        metadata = {
+          namespace = namespace;
+        };
+        data = {
           "uri" = "postgresql://postgres:${kubenix.lib.secretsFor "postgresql_admin_password"}+@postgresql-hl:5432/linkwarden";
         };
       };
