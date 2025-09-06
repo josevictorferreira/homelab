271b2b876111a44216710430f18d0e72368cb0ef
Jose Victor Ferreira <root@josevictor.me>
2025-09-06 13:24:22 -0300

    added openwebui, added openrouter secrets

---
+++ kubernetes/kubenix/apps/open-webui.nix
@@ -0,0 +1,96 @@
+{ lib, kubenix, k8sLib, homelab, ... }:
+
+let
+  app = "open-webui";
+  namespace = homelab.kubernetes.namespaces.apps;
+  bucketName = "open-webui-files";
+in
+{
+  kubernetes = {
+    helm.releases.${app} = {
+      chart = k8sLib.helm.fetch
+        {
+          repo = "https://helm.openwebui.com/";
+          chart = "open-webui";
+          version = "8.1.0";
+          sha256 = "sha256-nhvifpDdM8MoxF43cJAi6o+il2BbHX+udVAvvm1PukM=";
+        };
+      includeCRDs = true;
+      noHooks = true;
+      namespace = namespace;
+      values = {
+        image = {
+          repository = "ghcr.io/open-webui/open-webui";
+          pullPolicy = "IfNotPresent";
+        };
+        ollama.enabled = false;
+        openaiBaseApiUrls = [
+          "https://openrouter.ai/api/v1"
+        ];
+        ingress = {
+          host = k8sLib.domainFor app;
+          class = "cilium";
+          tls = true;
+          existingSecret = "wildcard-tls";
+        };
+        service = {
+          type = "LoadBalancer";
+          annotations = k8sLib.serviceIpFor app;
+          loadBalancerClass = "cilium";
+        };
+        persistence = {
+          enabled = true;
+          size = "2Gi";
+          storageClass = "rook-ceph-block";
+          provider = "s3";
+          s3 = {
+            accessKeyExistingSecret = "open-webui-s3";
+            accessKeyExistingAccessKey = "AWS_ACCESS_KEY_ID";
+            secretKeyExistingSecret = "open-webui-s3";
+            secretKeyExistingSecretKey = "AWS_SECRET_ACCESS_KEY";
+            endpointUrl = k8sLib.objectStoreEndpoint;
+            region = "us-east-1";
+            bucket = bucketName;
+          };
+        };
+        extraEnvVars = [
+          {
+            name = "ENABLE_SIGNUP";
+            value = "false";
+          }
+          {
+            name = "OPENAI_API_KEY";
+            valueFrom = {
+              secretKeyRef = {
+                name = "openrouter-secrets";
+                key = "OPENROUTER_API_KEY";
+              };
+            };
+          }
+          {
+            name = "OPENAI_API_BASE_URL";
+            valueFrom = {
+              secretKeyRef = {
+                name = "openrouter-secrets";
+                key = "OPENROUTER_API_BASE_URL";
+              };
+            };
+          }
+        ];
+      };
+    };
+
+    resources = {
+      objectbucketclaim."open-webui-s3" = {
+        metadata = {
+          namespace = namespace;
+        };
+        spec = {
+          bucketName = bucketName;
+          storageClassName = "rook-ceph-objectstore";
+        };
+      };
+    };
+
+  };
+}
