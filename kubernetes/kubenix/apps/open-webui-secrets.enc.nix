4f9dcdaad8d9e3b8fabadc3bba87334ba5a8fbcd
Jose Victor Ferreira <root@josevictor.me>
2025-09-06 16:18:16 -0300

    updated ingress for open webui

--- kubernetes/kubenix/apps/open-webui-secrets.enc.nix
+++ kubernetes/kubenix/apps/open-webui-secrets.enc.nix
@@ -15,6 +15,7 @@ in
           "OFFLINE_MODE" = "True";
           "ENABLE_WEBSOCKET_SUPPORT" = "True";
           "WEBSOCKET_MANAGER" = "True";
+          "ADMIN_EMAIL" = "root@josevictor.me";
           "DATABASE_URL" = "postgresql://postgres:${kubenix.lib.secretsFor "postgresql_admin_password"}+@postgresql-hl:5432/open-webui";
           "REDIS_URL" = "redis://:${kubenix.lib.secretsFor "redis_password"}+@redis-headless:6379/0";
           "WEBUI_SECRET_KEY" = kubenix.lib.secretsFor "openwebui_secret_key";
