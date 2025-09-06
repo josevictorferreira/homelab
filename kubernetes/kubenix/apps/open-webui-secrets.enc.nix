2cd78b3d58f0b92d144634e59d61ab132e78755f
Jose Victor Ferreira <root@josevictor.me>
2025-09-06 14:34:35 -0300

    added open webui secrets

---
+++ kubernetes/kubenix/apps/open-webui-secrets.enc.nix
@@ -0,0 +1,20 @@
+{ kubenix, homelab, ... }:
+
+let
+  namespace = homelab.kubernetes.namespaces.applications;
+in
+{
+  kubernetes = {
+    resources = {
+      secrets."open-webui-secrets" = {
+        metadata = {
+          namespace = namespace;
+        };
+        stringData = {
+          "DATABASE_URL" = "postgresql://postgres:${kubenix.lib.secretsFor "postgresql_admin_password"}+@postgresql-hl:5432/open-webui";
+          "REDIS_URL" = "redis://:${kubenix.lib.secretsFor "redis_password"}+@redis-headless:6379/0";
+        };
+      };
+    };
+  };
+}
