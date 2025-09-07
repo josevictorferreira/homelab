5c50608578a38d865175c5a4f3048afaa4cc1850
Jose Victor Ferreira <root@josevictor.me>
2025-09-06 22:25:22 -0300

    added uptime kuma service

---
+++ kubernetes/kubenix/apps/_ntfy.nix
@@ -0,0 +1,78 @@
+{ lib, kubenix, k8sLib, homelab, ... }:
+
+let
+  app = "ntfy";
+  namespace = homelab.kubernetes.namespaces.applications;
+in
+{
+  kubernetes = {
+    helm.releases.${app} = {
+      chart = k8sLib.helm.fetch {
+        repo = "https://fmjstudios.github.io/helm-charts/";
+        chart = "ntfy";
+        version = "0.2.2";
+        sha256 = "sha256-qFG0Iq2IBwkqG6t2Z47GDU3fjftzy3xI7ALNJjctNQk=";
+      };
+      includeCRDs = true;
+      noHooks = true;
+      namespace = namespace;
+
+      values = {
+        image = {
+          registry = "docker.io";
+          repository = "binwiederhier/ntfy";
+          tag = "v2.11.0";
+          pullPolicy = "IfNotPresent";
+        };
+
+        ntfy = {
+          baseURL = "ntfy.${homelab.domain}";
+          listenHTTP = ":80";
+          behindProxy = true;
+          web = {
+            publicKey = "\${VAPID_PUBLIC_KEY}";
+            privateKey = "\${VAPID_PRIVATE_KEY}";
+            file = "/data/webpush.db";
+            emailAddress = "alerts@${homelab.domain}";
+          };
+          data = {
+            rootPath = "/data";
+            pvc = {
+              size = "5Gi";
+              storageClass = "ceph-rbd";
+            };
+          };
+          upstream.baseURL = "https://ntfy.sh";
+          log.level = "info";
+          cache.file = "/data/cache.db";
+          attachment = {
+            cacheDir = "/data/attachments";
+            totalSizeLimit = "5G";
+            fileSizeLimit = "15M";
+            expiryDuration = "3h";
+          };
+        };
+
+        ingress = {
+          enabled = true;
+          className = "nginx";
+          annotations = {
+            "cert-manager.io/cluster-issuer" = "cloudflare-issuer";
+          };
+          tls = [
+            {
+              hosts = [ "ntfy.${homelab.domain}" ];
+              secretName = "wildcard-tls";
+            }
+          ];
+        };
+
+        extraEnvFrom = [
+          {
+            secretRef.name = "ntfy-secrets";
+          }
+        ];
+      };
+    };
+  };
+}
