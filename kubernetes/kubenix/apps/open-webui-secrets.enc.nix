e019b230cd853d86cc3a76e6cf79737eeb716fbb
Jose Victor Ferreira <root@josevictor.me>
2025-09-06 22:55:34 -0300

    readded open webui

--- kubernetes/kubenix/apps/open-webui-secrets.enc.nix
+++ kubernetes/kubenix/apps/open-webui-secrets.enc.nix
@@ -17,7 +17,7 @@ in
           "ENABLE_WEBSOCKET_SUPPORT" = "True";
           "WEBSOCKET_MANAGER" = "True";
           "ADMIN_EMAIL" = "root@josevictor.me";
-          "DATABASE_URL" = "postgresql://postgres:${kubenix.lib.secretsFor "postgresql_admin_password"}+@postgresql-hl:5432/open-webui";
+          "DATABASE_URL" = "postgresql://postgres:${kubenix.lib.secretsFor "postgresql_admin_password"}+@postgresql-hl:5432/openwebui";
           "REDIS_URL" = "redis://:${kubenix.lib.secretsFor "redis_password"}+@redis-headless:6379/0";
           "WEBUI_SECRET_KEY" = kubenix.lib.secretsFor "openwebui_secret_key";
         };
