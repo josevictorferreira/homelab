c195eeb202a760c27bb2b2b6bd3e882c44504a81
Jose Victor Ferreira <root@josevictor.me>
2025-09-08 14:21:54 -0300

    Add Nix configuration for searxng application
    Introduce the full Nix module for deploying the searxng search engine via Helm chart, specifying the repository, version, and sha256 sum, with default values for replica count, image, service, and ingress.

---
+++ kubernetes/kubenix/apps/searxng.nix
@@ -0,0 +1,35 @@
+{ k8sLib, homelab, ... }:
+
+let
+  namespace = homelab.kubernetes.namespaces.applications;
+  app = "searxng";
+in
+{
+  kubernetes = {
+    helm.releases.${app} = {
+      chart = k8sLib.helm.fetch {
+        repo = "https://self-hosters-by-night.github.io/helm-charts";
+        chart = "searxng";
+        version = "1.0.0";
+        sha256 = "sha256-rFzutBrDDF4qVj38dYazjv3iUl2uszIJSKWPwrRdX1E=";
+      };
+      includeCRDs = true;
+      noHooks = true;
+      namespace = namespace;
+
+      values = {
+        replicaCount = 1;
+
+        image = {
+          repository = "searxng/searxng";
+          tag = "searxng/searxng:2025.9.5-e7501ea@sha256:511fa3f34c1d119c47f88c9a1e7eda1340f346543e980c17e5f14b27dac6a1ed";
+        };
+
+        service = k8sLib.plainServiceFor app;
+
+        ingress = k8sLib.ingressDomainForService app;
+      };
+    };
+  };
+}
+
