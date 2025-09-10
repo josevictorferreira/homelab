aabb6a9eda834678168afab81124c4975be89ccd
Jose Victor Ferreira <root@josevictor.me>
2025-09-10 13:44:46 -0300

    Add encrypted secrets for applications and storage configurations; update Nix configurations for n8n and related manifests, ensuring proper secret integration and filesystem setup.

--- kubernetes/kubenix/apps/n8n-enc.enc.nix
+++ kubernetes/kubenix/apps/n8n-enc.enc.nix
@@ -13,6 +13,8 @@ in
         stringData = {
           "redis-password" = kubenix.lib.secretsFor "redis_password";
           "postgres-password" = kubenix.lib.secretsFor "postgresql_admin_password";
+          "access-key-id" = kubenix.lib.secretsFor "ceph_objectstore_access_key_id";
+          "secret-access-key" = kubenix.lib.secretsFor "ceph_objectstore_secret_access_key";
         };
       };
     };
