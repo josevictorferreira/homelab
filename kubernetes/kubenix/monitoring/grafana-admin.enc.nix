ad871f6253d955aa43614937cb6bae07e7e960b0
Jose Victor Ferreira <root@josevictor.me>
2025-09-27 12:51:33 -0300

    Update Grafana alerting contact points configuration to enable notifications via Ntfy webhook and enhance alerting integration in monitoring setup.

--- kubernetes/kubenix/monitoring/grafana-admin.enc.nix
+++ kubernetes/kubenix/monitoring/grafana-admin.enc.nix
@@ -2,29 +2,6 @@
 
 let
   namespace = homelab.kubernetes.namespaces.monitoring;
-  ntfyContactPoint = {
-    apiVersion = 1;
-    contactPoints = [
-      {
-        orgId = 1;
-        name = "Ntfy";
-        receivers = [
-          {
-            uid = "dezclgug3tb0ga";
-            type = "webhook";
-            settings = {
-              headers = {
-                "X-Template" = "grafana";
-              };
-              httpMethod = "POST";
-              url = "http://ntfy.apps.svc.cluster.local/homelab";
-            };
-            disableResolveMessage = false;
-          }
-        ];
-      }
-    ];
-  };
 in
 {
   kubernetes = {
@@ -38,16 +15,6 @@ in
           "ADMIN_USER" = kubenix.lib.secretsFor "grafana_admin_username";
           "ADMIN_PASSWORD" = kubenix.lib.secretsFor "grafana_admin_password";
         };
-      };
-
-      configMaps."grafana-alerting-contactpoints" = {
-        metadata = {
-          namespace = namespace;
-          labels = {
-            grafana_alert = "1";
-          };
-        };
-        data."contact-points.yaml" = kubenix.lib.toYamlStr ntfyContactPoint;
       };
     };
   };
