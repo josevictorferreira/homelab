acf27029611e70cf9ea68287e64afeb8532119d5
Jose Victor Ferreira <root@josevictor.me>
2025-09-30 23:23:56 -0300

    Update encrypted Immich configuration parameters for improved security and functionality
    Adjusts Redis connection details and machine learning URL encryption data to enhance security and ensure proper deployment of Immich app. The new secrets values support updated environment variables and better integration with the existing infrastructure.

--- kubernetes/kubenix/apps/immich-config.enc.nix
+++ kubernetes/kubenix/apps/immich-config.enc.nix
@@ -11,7 +11,9 @@ in
           namespace = namespace;
         };
         stringData = {
-          REDIS_URL = "redis://:${kubenix.lib.secretsInlineFor "redis_password"}@redis-headless:6379/3";
+					REDIS_HOSTNAME = "redis-headless";
+					REDIS_PASSWORD = kubenix.lib.secretsFor "redis_password";
+					REDIS_DBINDEX = "3";	
           DB_URL = "postgresql://postgres:${kubenix.lib.secretsInlineFor "postgresql_admin_password"}@postgresql-hl:5432/immich";
           IMMICH_MACHINE_LEARNING_URL = "http://immich-machine-learning:3003";
         };
