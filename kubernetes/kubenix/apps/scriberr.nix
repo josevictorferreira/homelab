94732845752cfa8abbc53fe195f696daf736a01c
Jose Victor Ferreira <root@josevictor.me>
2025-10-03 21:26:13 -0300

    Add Scriberr application deployment module
    Introduce a new kubenix app module for Scriberr, a self-hosted service, enabling its deployment via the infrastructure setup. This allows easy management and inclusion of Scriberr in the cluster's applications, aligning with the project's goal of self-hosting services.

---
+++ kubernetes/kubenix/apps/scriberr.nix
@@ -0,0 +1,32 @@
+{ kubenix, homelab, ... }:
+
+let
+  namespace = homelab.kubernetes.namespaces.applications;
+in
+{
+  submodules.instances.scriberr = {
+    submodule = "release";
+    args = {
+      namespace = namespace;
+      image = {
+        repository = "ghcr.io/rishikanthc/scriberr";
+        tag = "v1.1.0@sha256:99a0b29003e046c08e5aad102fb4a01d28a298dbd3889819bcfb760a75cbfef6";
+        pullPolicy = "IfNotPresent";
+      };
+      subdomain = "scriberr";
+      port = 8080;
+      values = {
+        service.main = {
+          type = "LoadBalancer";
+          annotations = kubenix.lib.serviceAnnotationFor "scriberr";
+          ports = {
+            http = {
+              enabled = true;
+              port = 8080;
+            };
+          };
+        };
+      };
+    };
+  };
+}
