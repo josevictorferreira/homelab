970e7353e08d86ec7f6798293a3809cc9708268e
Jose Victor Ferreira <root@josevictor.me>
2025-10-05 20:57:19 -0300

    Add Ntfy application deployment via Kubernetes manifests and Helm submodule integration
    Implement deployment of Ntfy notification server with updated Helm chart references, ingress configuration, and persistent storage setup. This facilitates better notification management in the homelab setup, aligning with existing cluster configurations and enabling secure access.

--- kubernetes/kubenix/_submodules/release.nix
+++ kubernetes/kubenix/_submodules/release.nix
@@ -1,160 +1,129 @@
-{ lib, kubenix, ... }: with lib;
+{ lib, kubenix, ... }:
+with lib;
 
 {
-  submodules.imports = [{
-    module = { config, ... }:
-      let cfg = config.submodule.args; in
-      {
-        imports = with kubenix.modules; [
-          helm
-          k8s
-          submodule
-        ];
+  submodules.imports = [
+    {
+      module =
+        { config, ... }:
+        let
+          cfg = config.submodule.args;
+        in
+        {
+          imports = with kubenix.modules; [
+            helm
+            k8s
+            submodule
+          ];
 
-        options.submodule.args = {
-          enable = mkEnableOption "a simple and generic helm release template";
+          options.submodule.args = {
+            enable = mkEnableOption "a simple and generic helm release template";
 
-          namespace = mkOption {
-            default = "default";
-            type = types.str;
-            description = "target namespace";
-          };
-
-          image = mkOption {
-            type = types.attrsOf types.str;
-            default = { };
-            description = "image to use for the release";
-          };
-
-          subdomain = mkOption {
-            type = types.str;
-            default = "";
-            description = "hostname subdomain";
-          };
-
-          port = mkOption {
-            type = types.int;
-            default = 80;
-            description = "container port";
-          };
-
-          replicas = mkOption {
-            type = types.int;
-            default = 1;
-            description = "number of replicas";
-          };
-
-          secretName = mkOption {
-            type = types.str;
-            default = "";
-            description = "secret name for application credentials and configs";
-          };
-
-          resources = mkOption {
-            type = types.attrsOf (types.attrsOf types.str);
-            default = {};
-            description = "resources limits to be applied in the release";
-          };
-
-          persistence = mkOption {
-            description = "attrset of persistent volumes";
-            default = { };
-            type = types.attrsOf
-              (types.submodule
-                ({ config, ... }: {
-                  options = {
-                    enabled = mkOption {
-                      type = types.bool;
-                      default = true;
-                    };
-                    size = mkOption {
-                      type = types.str;
-                      default = "1Gi";
-                    };
-                    accessMode = mkOption {
-                      type = types.str;
-                      default = "ReadWriteOnce";
-                    };
-                    storageClass = mkOption {
-                      type = types.str;
-                      default = "rook-ceph-block";
-                    };
-                    mountPath = mkOption {
-                      type = types.str;
-                      default = "/";
-                    };
-                    existingClaim = mkOption {
-                      type = types.str;
-                      default = "";
-                    };
-                    type = mkOption {
-                      type = types.str;
-                      default = "persistentVolumeClaim";
-                    };
-                    hostPath = mkOption {
-                      type = types.str;
-                      default = "";
-                    };
-                    name = mkOption {
-                      type = types.str;
-                      default = "${config._module.args.name}-pvc";
-                    };
-                    readOnly = mkOption {
-                      type = types.bool;
-                      default = false;
-                    };
-                    subPath = mkOption {
-                      type = types.str;
-                      default = "";
-                    };
-                  };
-                }));
-          };
-
-          config =
-            mkOption
-              {
-                type = types.attrs;
-                default = { };
-                description = "yaml in configmap";
-              };
-
-          values = mkOption {
-            type = types.attrs;
-            default = { };
-            description = "freeform release values";
-          };
-        };
-
-        config = {
-          submodule = {
-            name = "release";
-            passthru.kubernetes.objects = config.kubernetes.objects;
-          };
-
-          kubernetes.helm.releases.${config._module.args.name} = {
-            inherit (cfg) namespace;
-            chart = kubenix.lib.helm.fetch {
-              repo = "https://bjw-s-labs.github.io/helm-charts/";
-              chart = "app-template";
-              version = "4.2.0";
-              sha256 = "sha256-JhHJmGrvpmdHfADfM4M4mby64cSH6HO6VpKmeQfngJA=";
+            namespace = mkOption {
+              default = "default";
+              type = types.str;
+              description = "target namespace";
             };
-            values =
-              lib.mkMerge [
+
+            image = mkOption {
+              type = types.attrsOf types.str;
+              default = { };
+              description = "image to use for the release";
+            };
+
+            subdomain = mkOption {
+              type = types.str;
+              default = "";
+              description = "hostname subdomain";
+            };
+
+            port = mkOption {
+              type = types.int;
+              default = 80;
+              description = "container port";
+            };
+
+            replicas = mkOption {
+              type = types.int;
+              default = 1;
+              description = "number of replicas";
+            };
+
+            secretName = mkOption {
+              type = types.str;
+              default = "";
+              description = "secret name for application credentials and configs";
+            };
+
+            resources = mkOption {
+              type = types.attrsOf (types.attrsOf types.str);
+              default = { };
+              description = "resources limits to be applied in the release";
+            };
+
+            persistence = mkOption {
+              description = "attrset of persistent volumes";
+              default = {
+                enabled = false;
+                type = "persistentVolumeClaim";
+                storageClass = "rook-ceph-block";
+                size = "1Gi";
+                accessMode = "ReadWriteOnce";
+                globalMounts = [
+                  {
+                    path = "/data";
+                    readOnly = false;
+                  }
+                ];
+              };
+              type = types.attrs;
+            };
+
+            config = mkOption {
+              type = types.attrs;
+              default = { };
+              description = "yaml in configmap";
+            };
+
+            values = mkOption {
+              type = types.attrs;
+              default = { };
+              description = "freeform release values";
+            };
+          };
+
+          config = {
+            submodule = {
+              name = "release";
+              passthru.kubernetes.objects = config.kubernetes.objects;
+            };
+
+            kubernetes.helm.releases.${config._module.args.name} = {
+              inherit (cfg) namespace;
+              chart = kubenix.lib.helm.fetch {
+                repo = "https://bjw-s-labs.github.io/helm-charts/";
+                chart = "app-template";
+                version = "4.2.0";
+                sha256 = "sha256-JhHJmGrvpmdHfADfM4M4mby64cSH6HO6VpKmeQfngJA=";
+              };
+              values = lib.mkMerge [
                 {
-                  inherit (cfg) persistence;
                   controllers.main.replicas = cfg.replicas;
+                  persistence.main = cfg.persistence;
                   controllers.main.containers.main = {
                     image = cfg.image;
-                    ports = [{
-                      name = "http";
-                      containerPort = cfg.port;
-                      protocol = "TCP";
-                    }];
-                  } // optionalAttrs (cfg.secretName != "") {
+                    ports = [
+                      {
+                        name = "http";
+                        containerPort = cfg.port;
+                        protocol = "TCP";
+                      }
+                    ];
+                  }
+                  // optionalAttrs (cfg.secretName != "") {
                     envFrom = [ { secretRef.name = cfg.secretName; } ];
-                  } // optionalAttrs (cfg.resources != { }) {
+                  }
+                  // optionalAttrs (cfg.resources != { }) {
                     resources = cfg.resources;
                   };
                   service.main = {
@@ -170,18 +139,24 @@
                   ingress.main = {
                     enabled = cfg.subdomain != "";
                     className = "cilium";
-                    hosts = [{
-                      host = kubenix.lib.domainFor cfg.subdomain;
-                      paths = [{
-                        path = "/";
-                        service.name = "${config._module.args.name}";
-                        service.port = cfg.port;
-                      }];
-                    }];
-                    tls = [{
-                      secretName = "wildcard-tls";
-                      hosts = [ (kubenix.lib.domainFor cfg.subdomain) ];
-                    }];
+                    hosts = [
+                      {
+                        host = kubenix.lib.domainFor cfg.subdomain;
+                        paths = [
+                          {
+                            path = "/";
+                            service.name = "${config._module.args.name}";
+                            service.port = cfg.port;
+                          }
+                        ];
+                      }
+                    ];
+                    tls = [
+                      {
+                        secretName = "wildcard-tls";
+                        hosts = [ (kubenix.lib.domainFor cfg.subdomain) ];
+                      }
+                    ];
                   };
                 }
                 (mkIf (cfg.config != { }) {
@@ -198,8 +173,9 @@
                 })
                 cfg.values
               ];
+            };
           };
         };
-      };
-  }];
+    }
+  ];
 }
