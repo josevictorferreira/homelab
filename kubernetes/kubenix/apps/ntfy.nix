efd7f818ec2599417a8afe1e75b3e03c43046d0c
Jose Victor Ferreira <root@josevictor.me>
2025-10-05 23:07:35 -0300

    Create initial configuration and manifests for Ntfy app
    Set up the `release.nix` module to support YAML and JSON configs, enabling flexible secret and config management.
    Update `blocky`, `glance`, and `ntfy` modules to include default configuration files and mounting options.
    Add corresponding Helm YAML manifests for `blocky`, `glance`, and `ntfy`, including ConfigMaps with their configurations.
    This facilitates deploying and managing these applications within the Kubernetes cluster.

--- kubernetes/kubenix/apps/ntfy.nix
+++ kubernetes/kubenix/apps/ntfy.nix
@@ -1,10 +1,11 @@
-{ homelab, ... }:
+{ homelab, kubenix, ... }:
 
 let
   namespace = homelab.kubernetes.namespaces.applications;
+  app = "ntfy";
 in
 {
-  submodules.instances.ntfy = {
+  submodules.instances.${app} = {
     submodule = "release";
     args = {
       namespace = namespace;
@@ -13,7 +14,7 @@ in
         tag = "v2.14.0@sha256:5a051798d14138c3ecb12c038652558ab6a077e1aceeb867c151cbf5fa8451ef";
         pullPolicy = "IfNotPresent";
       };
-      subdomain = "ntfy";
+      subdomain = app;
       port = 80;
       secretName = "ntfy-secrets";
       command = [
@@ -34,6 +35,37 @@ in
             readOnly = false;
           }
         ];
+      };
+      config = {
+        filename = "config.yml";
+        mountPath = "/var/lib/ntfy";
+        data = {
+          base-url = "https://${kubenix.lib.domainFor app}";
+          listen-http = ":80";
+          web-root = "/";
+          global-topic-limit = 15000;
+          behind-proxy = true;
+          cache-file = "/data/cache.db";
+          auth-default-access = "deny-all";
+          attachment-cache-dir = "/data/attachments";
+          attachment-total-size-limit = "5G";
+          attachment-file-size-limit = "15M";
+          attachment-expiry-duration = "3h";
+          web-push-file = "/data/webpush.db";
+          web-push-email-address = "alerts@josevictor.me";
+          upstream-base-url = "https://ntfy.sh";
+          visitor-subscription-limit = 30;
+          visitor-request-limit-burst = 60;
+          visitor-request-limit-replenish = "5s";
+          visitor-message-daily-limit = 15000;
+          visitor-email-limit-burst = 16;
+          visitor-email-limit-replenish = "1h";
+          visitor-attachment-total-size-limit = "100M";
+          visitor-attachment-daily-bandwidth-limit = "500M";
+          metrics-listen-http = ":9090";
+          log-level = "info";
+          log-format = "text";
+        };
       };
     };
   };
