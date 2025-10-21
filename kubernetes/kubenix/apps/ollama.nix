538c5d682aaa24c19d511fdc1f94c62474ca63a3
Jose Victor Ferreira <root@josevictor.me>
2025-10-21 11:05:34 -0300

    feat: Update Ollama to version 9.4.2
    Ollama was updated to the latest stable version to benefit from performance improvements and bug fixes.

--- kubernetes/kubenix/apps/ollama.nix
+++ kubernetes/kubenix/apps/ollama.nix
@@ -48,7 +48,7 @@ in
         extraEnv = [
           {
             name = "HSA_OVERRIDE_GFX_VERSION";
-            value = "9.0.a";
+            value = "9.4.2";
           }
           {
             name = "OLLAMA_DEBUG";
