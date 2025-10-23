4f04b335965da1f14ae937a56470b7b9d9e14a36
Jose Victor Ferreira <root@josevictor.me>
2025-10-23 20:29:06 -0300

    feat: Update openrouter-proxy to v0.0.4
    Updates the `openrouter-proxy` deployment to use the latest `v0.0.4` tag. This ensures we are running the most recent version of the proxy, potentially incorporating bug fixes or new features.

--- kubernetes/kubenix/apps/openrouter-proxy.nix
+++ kubernetes/kubenix/apps/openrouter-proxy.nix
@@ -10,7 +10,7 @@ in
       namespace = "apps";
       image = {
         repository = "ghcr.io/josevictorferreira/openrouter-proxy";
-        tag = "v0.0.3@sha256:ac96756841bc22c2b573727404d75047eec93dc9e50cbb8778daf2205dc1b899";
+        tag = "v0.0.4";
         pullPolicy = "IfNotPresent";
       };
       port = 8080;
