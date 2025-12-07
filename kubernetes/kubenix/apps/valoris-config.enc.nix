5db0db442e8028fc5b1237bbd63414db352b2bf0
Jose Victor Ferreira <root@josevictor.me>
2025-12-06 21:11:30 -0300

    Valoris update

--- kubernetes/kubenix/apps/valoris-config.enc.nix
+++ kubernetes/kubenix/apps/valoris-config.enc.nix
@@ -16,6 +16,8 @@ in
           "VALORIS_DATABASE_PASSWORD" = kubenix.lib.secretsFor "postgresql_admin_password";
           "VALORIS_JOBS_UI_USERNAME" = kubenix.lib.secretsFor "valoris_jobs_ui_username";
           "VALORIS_JOBS_UI_PASSWORD" = kubenix.lib.secretsFor "valoris_jobs_ui_password";
+          "VALORIS_RES_BROWSER_USERNAME" = kubenix.lib.secretsFor "valoris_res_browser_username";
+          "VALORIS_RES_BROWSER_PASSWORD" = kubenix.lib.secretsFor "valoris_res_browser_password";
           "SECRET_KEY_BASE" = kubenix.lib.secretsFor "valoris_secret_key_base";
         };
       };
