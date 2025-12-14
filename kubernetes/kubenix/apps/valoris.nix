bf41db899ce1c583d141e17a31845769fcd2bdf1
Jose Victor Ferreira <root@josevictor.me>
2025-12-14 10:32:30 -0300

    💄 **Configure Valoris for S3 storage and debug logging**
    Configure Valoris to use S3 for storage and enable debug logging. This change ensures proper functioning of Valoris with object storage and provides enhanced diagnostic capabilities.

--- kubernetes/kubenix/apps/valoris.nix
+++ kubernetes/kubenix/apps/valoris.nix
@@ -1,43 +1,70 @@
-{ ... }:
+{ homelab, ... }:
 
 let
   imageTag = "latest";
+  namespace = homelab.kubernetes.namespaces.applications;
+  bucketName = "valoris-s3";
+  secretName = "valoris-config";
 in
 {
   submodules.instances = {
     valoris = {
       submodule = "release";
       args = {
-        namespace = "apps";
+        namespace = namespace;
         image = {
           repository = "ghcr.io/josevictorferreira/valoris-server";
           tag = imageTag;
           pullPolicy = "Always";
         };
-        secretName = "valoris-config";
         port = 3000;
         values = {
           defaultPodOptions.imagePullSecrets = [
             { name = "ghcr-registry-secret"; }
           ];
+          controllers.main.containers.main = {
+            envFrom = [
+              { secretRef.name = secretName; }
+              { secretRef.name = "valoris-s3"; }
+            ];
+          };
         };
       };
     };
     valoris-worker = {
       submodule = "release";
       args = {
-        namespace = "apps";
+        namespace = namespace;
         image = {
           repository = "ghcr.io/josevictorferreira/valoris-worker";
           tag = imageTag;
           pullPolicy = "Always";
         };
-        secretName = "valoris-config";
         port = 3000;
         values = {
           defaultPodOptions.imagePullSecrets = [
             { name = "ghcr-registry-secret"; }
           ];
+          controllers.main.containers.main = {
+            envFrom = [
+              { secretRef.name = secretName; }
+              { secretRef.name = "valoris-s3"; }
+            ];
+          };
+        };
+      };
+    };
+  };
+
+  kubernetes = {
+    resources = {
+      objectbucketclaim."linkwarden-s3" = {
+        metadata = {
+          namespace = namespace;
+        };
+        spec = {
+          bucketName = bucketName;
+          storageClassName = "rook-ceph-objectstore";
         };
       };
     };
