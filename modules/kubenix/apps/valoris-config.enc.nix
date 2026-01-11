39d06a198f1122dcdf330c4644223f686d395a51
Jose Victor Ferreira <root@josevictor.me>
2026-01-11 17:27:16 -0300

    💄 Update Valoris and imgproxy configurations Integrate imgproxy and update Valoris application secrets and configuration to support image proxying.

--- modules/kubenix/apps/valoris-config.enc.nix
+++ modules/kubenix/apps/valoris-config.enc.nix
@@ -24,6 +24,9 @@ in
             "http://rook-ceph-rgw-ceph-objectstore.${homelab.kubernetes.namespaces.storage}.svc.cluster.local";
           "S3_PUBLIC_ENDPOINT" = "https://objectstore.josevictor.me";
           "S3_BUCKET" = "valoris-s3";
+          "IMGPROXY_ENDPOINT" = "http://imgproxy-imgproxy.apps.svc.cluster.local";
+          "IMGPROXY_KEY" = kubenix.lib.secretsFor "imgproxy_key";
+          "IMGPROXY_SALT" = kubenix.lib.secretsFor "imgproxy_salt";
         };
       };
     };
