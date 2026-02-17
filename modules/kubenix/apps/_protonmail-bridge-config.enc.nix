0c5c11d598c497b7c09e3188b4eb09d2491da11b
Jose Victor Ferreira <root@josevictor.me>
2026-02-17 10:59:03 -0300

    feat 🚀 Remove Protonmail Bridge and update storage request
    The Protonmail Bridge application has been removed from the cluster. The storage request for the Persistent Volume Claim has been increased from 1Gi to 10Gi to accommodate expected growth for other applications.

--- modules/kubenix/apps/_protonmail-bridge-config.enc.nix
+++ modules/kubenix/apps/_protonmail-bridge-config.enc.nix
@@ -1,10 +1,6 @@
 { kubenix, homelab, ... }:
 
 {
-  imports = [
-    ./protonmail-bridge.nix
-  ];
-
   # ProtonMail Bridge uses interactive CLI authentication
   # No additional secrets or configuration needed here
 }
