e1b8c18593b2214d7f57cac931a286eb475cb2fe
Jose Victor Ferreira <root@josevictor.me>
2026-02-22 20:41:17 -0300

    feat: Relocate Proton Drive backup to 'backup' namespace
    The Proton Drive backup tasks and configurations have been moved from the 'apps' namespace to the 'backup' namespace. This change organizes backup-specific workloads into a dedicated namespace for better clarity and management.

--- modules/kubenix/backup/shared-subfolders-proton-sync-config.enc.nix
+++ modules/kubenix/backup/shared-subfolders-proton-sync-config.enc.nix
@@ -1,12 +1,7 @@
-{
-  kubenix,
-  lib,
-  pkgs,
-  ...
-}:
+{ kubenix, homelab, pkgs, ... }:
 let
   name = "shared-subfolders-proton-sync";
-  namespace = "apps";
+  namespace = homelab.kubernetes.namespaces.backup;
 in
 {
   kubernetes.resources.secrets.${name} = {
@@ -15,11 +10,8 @@ in
       inherit namespace;
     };
     stringData = {
-      KEYRING_PASSWORD = kubenix.lib.secretsFor "proton_drive_keyring_password";
       PROTON_USERNAME = kubenix.lib.secretsFor "proton_drive_username";
       PROTON_PASSWORD = kubenix.lib.secretsFor "proton_drive_password";
-      MINIO_ACCESS_KEY_ID = kubenix.lib.secretsFor "minio_shared_backup_access_key_id";
-      MINIO_SECRET_ACCESS_KEY = kubenix.lib.secretsFor "minio_shared_backup_secret_access_key";
     };
   };
 }
