2c33e79cb58c62112a9142610437ffa62c9a2d8f
Jose Victor Ferreira <root@josevictor.me>
2026-02-25 12:50:32 -0300

    fix: point OPENCLAW_DATA_DIR to cephfs mount at /home/node/.openclaw

--- modules/kubenix/apps/openclaw-nix.nix
+++ modules/kubenix/apps/openclaw-nix.nix
@@ -271,7 +271,7 @@ in
 
         # Environment overrides
         controllers.main.containers.main.env.OPENCLAW_CONFIG_PATH = "/config/openclaw.json";
-        controllers.main.containers.main.env.OPENCLAW_DATA_DIR = "/state/openclaw";
+        controllers.main.containers.main.env.OPENCLAW_DATA_DIR = "/home/node/.openclaw";
         controllers.main.containers.main.env.HOME = "/state/home";
         controllers.main.containers.main.env.TZ = "America/Sao_Paulo";
         controllers.main.containers.main.env.NODE_PATH = "/lib/openclaw/extensions/matrix/node_modules";
