24e8701cb9f9ef1b6e954736c3df30ca463c1960
Jose Victor Ferreira <root@josevictor.me>
2026-03-20 18:09:27 -0300

    chore: remove Dendrite (matrixx) - using Synapse only

--- config/kubernetes.nix
+++ config/kubernetes.nix
@@ -48,7 +48,7 @@
       rabbitmq = "10.10.10.139";
       openclaw = "10.10.10.140";
       openclaw-nix = "10.10.10.141";
-      matrixx = "10.10.10.142";
+
       nfs = "10.10.10.150";
       grafana = "10.10.10.190";
     };
@@ -67,7 +67,6 @@
       "mautrix_slack"
       "mautrix_discord"
       "mautrix_whatsapp"
-      "dendrite"
     ];
   };
 
