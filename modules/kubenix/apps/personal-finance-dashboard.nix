7bb9272dcd56b256c12b0a57a2149a0aea6f85b2
Jose Victor Ferreira <root@josevictor.me>
2026-04-19 14:16:36 -0300

    feat: Update personal finance dashboard image
    Updates the container image for the personal finance dashboard to the latest version. This ensures the application is running with the most recent features and security patches.

--- modules/kubenix/apps/personal-finance-dashboard.nix
+++ modules/kubenix/apps/personal-finance-dashboard.nix
@@ -24,7 +24,7 @@ in
             containers = [
               {
                 inherit name;
-                image = "ghcr.io/josevictorferreira/personal-finances@sha256:9cacadbff282224b7b1143e5e3394797e3e2e09e4d38b9adda0083477686ded4";
+                image = "ghcr.io/josevictorferreira/personal-finances@sha256:80cf3082cb683d71324426b410e465abc794d6e9b87dc2f64992cef04bac104b";
                 imagePullPolicy = "IfNotPresent";
                 ports = [
                   {
