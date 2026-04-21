91b2797ad4f0ed3d6c6e25bdf9a79a0dc30ef735
Jose Victor Ferreira <root@josevictor.me>
2026-04-21 13:50:46 -0300

    chore(keycloak): bump valoris-identity to v0.3.0 (login fixes)

--- modules/kubenix/apps/keycloak.nix
+++ modules/kubenix/apps/keycloak.nix
@@ -138,7 +138,7 @@ in
           }
           {
             name = "add-valoris-theme";
-            image = "ghcr.io/josevictorferreira/valoris-identity:v0.2.0";
+            image = "ghcr.io/josevictorferreira/valoris-identity:v0.3.0";
             command = [
               "sh"
               "-c"
