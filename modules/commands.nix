feecc587215fb748697e10521842328b31a3afea
Jose Victor Ferreira <root@josevictor.me>
2026-05-12 22:42:18 -0300

    feat: upgrade openclaw-nix to 2026.5.12-beta.3

--- modules/commands.nix
+++ modules/commands.nix
@@ -555,7 +555,7 @@ let
 
   # OpenClaw image configuration
   openclawImageName = "openclaw-nix";
-  openclawVersion = "2026.5.12-beta.2";
+  openclawVersion = "2026.5.12-beta.3";
   openclawRegistry = "ghcr.io";
 
   push-openclaw =
