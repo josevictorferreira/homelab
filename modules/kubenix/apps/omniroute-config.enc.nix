d2b7539ee1735219a686ec772019f9b6014bdb10
Jose Victor Ferreira <root@josevictor.me>
2026-05-30 14:49:30 -0300

    Add omniroute (3.8.7) - diegosouzapw fork of 9router

---
+++ modules/kubenix/apps/omniroute-config.enc.nix
@@ -0,0 +1,30 @@
+{ kubenix, homelab, ... }:
+
+let
+  namespace = homelab.kubernetes.namespaces.applications;
+  app = "omniroute";
+in
+{
+  kubernetes.resources.secrets."${app}-env" = {
+    metadata.namespace = namespace;
+    stringData = {
+      JWT_SECRET = kubenix.lib.secretsFor "omniroute_jwt_secret";
+      INITIAL_PASSWORD = kubenix.lib.secretsFor "omniroute_initial_password";
+      API_KEY_SECRET = kubenix.lib.secretsFor "omniroute_api_key_secret";
+      MACHINE_ID_SALT = kubenix.lib.secretsFor "omniroute_machine_id_salt";
+      DATA_DIR = "/app/data";
+      PORT = "20128";
+      NODE_ENV = "production";
+      HOSTNAME = "0.0.0.0";
+      NEXT_TELEMETRY_DISABLED = "1";
+      BASE_URL = "http://localhost:20128";
+      NEXT_PUBLIC_BASE_URL = "http://localhost:20128";
+      CLOUD_URL = "https://9router.com";
+      NEXT_PUBLIC_CLOUD_URL = "https://9router.com";
+      ENABLE_REQUEST_LOGS = "false";
+      OBSERVABILITY_ENABLED = "true";
+      AUTH_COOKIE_SECURE = "false";
+      REQUIRE_API_KEY = "false";
+    };
+  };
+}
---
+++ modules/kubenix/apps/omniroute.nix
@@ -0,0 +1,66 @@
+{ kubenix, homelab, ... }:
+
+let
+  namespace = homelab.kubernetes.namespaces.applications;
+  app = "omniroute";
+in
+{
+  submodules.instances.${app} = {
+    submodule = "release";
+    args = {
+      inherit namespace;
+      image = {
+        repository = "ghcr.io/diegosouzapw/omniroute";
+        tag = "3.8.7@sha256:ca0ec278681d1ad8d331f6b6f0a45f1ded7773e45bbc102c97f897e8d6ff7e4b";
+        pullPolicy = "IfNotPresent";
+      };
+      secretName = "${app}-env";
+      port = 20128;
+      resources = {
+        limits = {
+          cpu = "500m";
+          memory = "512Mi";
+        };
+        requests = {
+          cpu = "250m";
+          memory = "256Mi";
+        };
+      };
+
+      values = {
+        controllers.main.strategy = "Recreate";
+
+        defaultPodOptions.imagePullSecrets = [
+          { name = "ghcr-registry-secret"; }
+        ];
+
+        persistence.data = {
+          enabled = true;
+          type = "persistentVolumeClaim";
+          storageClass = kubenix.lib.defaultStorageClass;
+          size = "1Gi";
+          accessMode = "ReadWriteOnce";
+          globalMounts = [
+            {
+              path = "/app/data";
+              readOnly = false;
+            }
+          ];
+        };
+        persistence.data-home = {
+          enabled = true;
+          type = "persistentVolumeClaim";
+          storageClass = kubenix.lib.defaultStorageClass;
+          size = "1Gi";
+          accessMode = "ReadWriteOnce";
+          globalMounts = [
+            {
+              path = "/app/data-home";
+              readOnly = false;
+            }
+          ];
+        };
+      };
+    };
+  };
+}
