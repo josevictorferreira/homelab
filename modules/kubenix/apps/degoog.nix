a140f4cfef3f65e3f8c7a6fe1c240d2e8cdaa788
Jose Victor Ferreira <root@josevictor.me>
2026-06-12 23:03:21 -0300

    feat(degoog): add degoog search proxy service
    - Add kubenix module using release submodule (bjw-s app-template)
    - Image: ghcr.io/degoog-org/degoog:latest with pinned digest
    - Port 4444, LoadBalancer IP 10.10.10.151
    - PVC: 1Gi rook-ceph-block at /app/data
    - Ingress at degoog.josevictor.me

---
+++ modules/kubenix/apps/degoog.nix
@@ -0,0 +1,43 @@
+{ homelab, kubenix, ... }:
+
+let
+  namespace = homelab.kubernetes.namespaces.applications;
+  app = "degoog";
+in
+{
+  submodules.instances.${app} = {
+    submodule = "release";
+    args = {
+      inherit namespace;
+      image = {
+        repository = "ghcr.io/degoog-org/degoog";
+        tag = "latest@sha256:4b3ba6fe7ab6c5ca76ce8aedbd5d07da7423d1043613fae94ff9f1c8acced479";
+        pullPolicy = "Always";
+      };
+      port = 4444;
+      resources = {
+        requests = {
+          cpu = "100m";
+          memory = "128Mi";
+        };
+        limits = {
+          cpu = "500m";
+          memory = "512Mi";
+        };
+      };
+      persistence = {
+        enabled = true;
+        type = "persistentVolumeClaim";
+        storageClass = kubenix.lib.defaultStorageClass;
+        size = "1Gi";
+        accessMode = "ReadWriteOnce";
+        globalMounts = [
+          {
+            path = "/app/data";
+            readOnly = false;
+          }
+        ];
+      };
+    };
+  };
+}
