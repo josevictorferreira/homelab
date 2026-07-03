4e09136e5dedb0a33c2f1062ddab50e8604fc562
Jose Victor Ferreira <root@josevictor.me>
2026-07-03 12:48:30 -0300

    feat(apps): deploy Home Assistant at hass.josevictor.me

---
+++ modules/kubenix/apps/home-assistant.nix
@@ -0,0 +1,82 @@
+{ kubenix, homelab, ... }:
+
+let
+  namespace = homelab.kubernetes.namespaces.applications;
+  app = "home-assistant";
+  domain = "hass.${homelab.domain}";
+in
+{
+  kubernetes = {
+    helm.releases.${app} = {
+      chart = kubenix.lib.helm.fetch {
+        repo = "https://pajikos.github.io/home-assistant-helm-chart";
+        chart = "home-assistant";
+        version = "0.3.68";
+        sha256 = "sha256-/YUEnJAaH6Ea1IeGmaHy2zqdr8nN3xPp11eczVIzrXY=";
+      };
+      noHooks = true;
+      inherit namespace;
+
+      values = {
+        replicaCount = 1;
+
+        image = {
+          repository = "ghcr.io/home-assistant/home-assistant";
+          tag = "2026.7.0";
+          pullPolicy = "IfNotPresent";
+        };
+
+        controller = {
+          type = "StatefulSet";
+        };
+
+        service = {
+          type = "ClusterIP";
+          port = 8080;
+        };
+
+        ingress = {
+          enabled = true;
+          className = kubenix.lib.defaultIngressClass;
+          annotations = {
+            "cert-manager.io/cluster-issuer" = kubenix.lib.defaultClusterIssuer;
+          };
+          hosts = [
+            {
+              host = domain;
+              paths = [
+                {
+                  path = "/";
+                  pathType = "Prefix";
+                }
+              ];
+            }
+          ];
+          tls = [
+            {
+              hosts = [ domain ];
+              secretName = kubenix.lib.defaultTLSSecret;
+            }
+          ];
+        };
+
+        persistence = {
+          enabled = true;
+          accessMode = "ReadWriteOnce";
+          size = "10Gi";
+          storageClass = kubenix.lib.defaultStorageClass;
+        };
+
+        configuration = {
+          enabled = true;
+          trusted_proxies = [
+            "10.0.0.0/8"
+            "172.16.0.0/12"
+            "192.168.0.0/16"
+            "127.0.0.0/8"
+          ];
+        };
+      };
+    };
+  };
+}
