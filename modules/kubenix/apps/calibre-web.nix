a8348e3521e9855e0dad3d0cd85805d8ee3c5bd8
Jose Victor Ferreira <root@josevictor.me>
2026-07-06 17:55:57 -0300

    feat(apps): add calibre-web to apps namespace
    - release submodule, linuxserver/calibre-web pinned by digest, port 8083
    - /config on rook-ceph-block PVC (5Gi), /books from shared CephFS root PVC
    - loadBalancer IP 10.10.10.153

---
+++ modules/kubenix/apps/calibre-web.nix
@@ -0,0 +1,61 @@
+{ homelab, kubenix, ... }:
+let
+  namespace = homelab.kubernetes.namespaces.applications;
+  app = "calibre-web";
+in
+{
+  submodules.instances.${app} = {
+    submodule = "release";
+    args = {
+      inherit namespace;
+      image = {
+        repository = "lscr.io/linuxserver/calibre-web";
+        tag = "latest@sha256:18cd5d1d5c13b133fdf25506df8db415aee675ecf3ea01d086f01296a39666c4";
+        pullPolicy = "IfNotPresent";
+      };
+      port = 8083;
+      resources = {
+        requests = {
+          cpu = "100m";
+          memory = "256Mi";
+        };
+        limits = {
+          cpu = "1";
+          memory = "1Gi";
+        };
+      };
+      persistence = {
+        enabled = true;
+        type = "persistentVolumeClaim";
+        storageClass = kubenix.lib.defaultStorageClass;
+        size = "5Gi";
+        accessMode = "ReadWriteOnce";
+        globalMounts = [
+          {
+            path = "/config";
+            readOnly = false;
+          }
+        ];
+      };
+      values = {
+        controllers.main.containers.main.env = {
+          PUID = "1000";
+          PGID = "1000";
+          TZ = homelab.timeZone;
+        };
+        persistence.books = {
+          enabled = true;
+          type = "persistentVolumeClaim";
+          existingClaim = kubenix.lib.sharedStorage.rootPVC;
+          globalMounts = [
+            {
+              path = "/books";
+              subPath = "books";
+              readOnly = false;
+            }
+          ];
+        };
+      };
+    };
+  };
+}
