024d8d07f26562138f59c1c672911ae68e9ba9a4
Jose Victor Ferreira <root@josevictor.me>
2026-07-08 20:15:28 -0300

    fix: inject sandbox-nix authorized key via encrypted secret

--- modules/kubenix/apps/sandbox-nix.nix
+++ modules/kubenix/apps/sandbox-nix.nix
@@ -21,15 +21,6 @@ let
   ];
 in
 {
-  kubernetes.resources.secrets."${name}-ssh" = {
-    metadata.namespace = namespace;
-    stringData = {
-      # Public key is mounted as authorized_keys so the private key owner
-      # (hermes agents) can authenticate.
-      "authorized_keys" = kubenix.lib.secretsFor "sandbox_nix_ssh_public_key";
-    };
-  };
-
   kubernetes.resources.statefulSets.${name} = {
     metadata = {
       inherit namespace;
