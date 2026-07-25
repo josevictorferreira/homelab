4bd3080894b38d0dc9f5c1756d6b30ec425f7380
Jose Victor Ferreira <root@josevictor.me>
2026-07-25 18:38:20 -0300

    fix(keycloak): bump oratoria-identity to v0.7.7 (force cache refresh)

--- modules/kubenix/apps/keycloak.nix
+++ modules/kubenix/apps/keycloak.nix
@@ -153,7 +153,7 @@ in
           }
           {
             name = "add-oratoria-theme";
-            image = "ghcr.io/josevictorferreira/oratoria-identity:v0.7.6";
+            image = "ghcr.io/josevictorferreira/oratoria-identity:v0.7.7";
             command = [
               "sh"
               "-c"
